7d0178fa429ef1f3768f8d740c1a3152a36b7f1d
Nicolas Serf <serf.nicolas@gmail.com>
2020-03-19 23:42:58 +0100

    [MISC] Modify batch script 1. Removed old code 2. Remove echo testing purpose

--- VaultsCoalition/InitializationSubmodules.bat
+++ VaultsCoalition/InitializationSubmodules.bat
@@ -53,7 +53,6 @@ if not exist %currentFolder%\%SUBMODULE_FOLDER_NAME%\%SUBMODULE_SCYLLA_NAME% (
 cd %pluginsFolder%
 if exist %CD%\%SUBMODULE_SCYLLA_NAME% (
 	@echo ==== Removing existing symlink
-	@echo %CD%\%SUBMODULE_SCYLLA_NAME%
 	rd %CD%\%SUBMODULE_SCYLLA_NAME%
 )
 
@@ -67,27 +66,5 @@ if not exist %CD%\%SUBMODULE_SCYLLA_NAME% (
 @echo ======================================================
 @echo == Initializing submodules for %PROJECT_NAME% - DONE
 @echo ======================================================
-:: Creating submodule folder
-
-::@echo Creating submodules folder
-::if exist %currentFolder%\%SUBMODULE_FOLDER_NAME% echo Folder %SUBMODULE_FOLDER_NAME% already exists - Skipping creation
-::if not exist %currentFolder%\%SUBMODULE_FOLDER_NAME% mkdir %currentFolder%\%SUBMODULE_FOLDER_NAME%
-
-:: Adding submodule
-
-::@echo Adding submodule %SUBMODULE_SCYLLA_NAME% at path %SUBMODULE_SCYLLA_PATH%
-::cd %SUBMODULE_FOLDER_NAME%
-::git submodule add --force %SUBMODULE_SCYLLA_PATH%
-::cd ..
-::@echo Adding submodule %SUBMODULE_SCYLLA_NAME% done
-
-:: Adding the Symlink
-
-::@echo Adding Symlink
-::cd %pluginsFolder%
-
-::rd %CD%\%SUBMODULE_SCYLLA_NAME%
-::mklink /d %CD%\%SUBMODULE_SCYLLA_NAME% %currentFolder%\%SUBMODULE_FOLDER_NAME%\%SUBMODULE_SCYLLA_NAME%\%SUBMODULE_SCYLLA_NAME%\%FOLDER_ASSET%
-::@echo Adding Symlink done
 
 pause > nul
